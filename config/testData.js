2c3c30831eaf9fec93e5bd9b1852640398237cb9
ra939 <ra939@students.waikato.ac.nz>
2026-03-20 22:08:22 +1300

    initial commit with testcase includes add order to cart and checkout

---
+++ config/testData.js
@@ -0,0 +1,5 @@
+export const config = {
+  baseURL: 'https://www.saucedemo.com/',
+  username: 'standard_user',
+  password: 'secret_sauce',
+};
---
+++ tests/orderManagement/addOrderToCart.spec.js
@@ -0,0 +1,66 @@
+import { test, expect } from "@playwright/test";
+
+const firstNameData = "Rashmi";
+const lastNameData = "Alagiyawanna";
+const postalCodeData = "3216";
+const orderNameData = "Sauce Labs Backpack";
+const checkoutCompleteText = "Checkout: Complete!";
+const checkoutCompleteHeader = '[data-test="secondary-header"]';
+const checkoutInformationText = "Checkout: Your Information";
+const titleyourCart = "Your Cart";
+const DashboardTitle = "Swag Labs";
+const username = '[data-test="username"]';
+const password = '[data-test="password"]';
+const loginButton = '[data-test="login-button"]';
+const firstName = '[data-test="firstName"]';
+const lastName = '[data-test="lastName"]';
+const postalCode = '[data-test="postalCode"]';
+const continueButton = '[data-test="continue"]';
+const finishButton = '[data-test="finish"]';
+const logoutButton = '[data-test="logout-sidebar-link"]';
+const backToProductsButton = '[data-test="back-to-products"]';
+const orderName =
+  '[data-test="item-4-title-link"] [data-test="inventory-item-name"]';
+const pageTitle = '[data-test="title"]';
+const orderTile = '[data-test="add-to-cart-sauce-labs-backpack"]';
+const shoppingCartLink = '[data-test="shopping-cart-link"]';
+const checkoutButton = '[data-test="checkout"]';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(config.baseURL);
+  await expect(page.getByText(DashboardTitle)).toBeVisible();
+  await page.locator(username).click();
+  await page.locator(username).fill(config.username);
+  await page.locator(password).click();
+  await page.locator(password).fill(config.password);
+  await page.locator(loginButton).click();
+  await expect(page.locator(pageTitle)).toBeVisible();
+});
+
+test("test add order to cart @sanity", async ({ page }) => {
+  await expect(page.locator(orderName)).toContainText(orderNameData);
+  await page.locator(orderTile).click();
+  await page.locator(shoppingCartLink).click();
+  await expect(page.locator(pageTitle)).toContainText(titleyourCart);
+  await expect(page.locator(checkoutButton)).toBeVisible();
+  await page.locator(checkoutButton).click();
+  await page.locator(firstName).click();
+  await page.locator(firstName).fill(firstNameData);
+  await page.locator(lastName).click();
+  await page.locator(lastName).fill(lastNameData);
+  await page.locator(postalCode).click();
+  await page.locator(postalCode).fill(postalCodeData);
+  await expect(page.locator(pageTitle)).toContainText(checkoutInformationText);
+  await page.locator(continueButton).click();
+  await expect(page.locator(pageTitle)).toBeVisible();
+  await page.locator(finishButton).click();
+  await expect(page.locator(checkoutCompleteHeader)).toContainText(
+    checkoutCompleteText,
+  );
+  await page.locator(backToProductsButton).click();
+});
+
+test.afterEach(async ({ page }) => {
+  await page.getByRole("button", { name: "Open Menu" }).click();
+  await page.locator(logoutButton).click();
+});
